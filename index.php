8cdceeb06b8c494c98a0a109fdb9291f2f3afb0d
ravisd2778 <ravisd143@gmail.com>
2019-03-12 10:52:52 +0530

    first

---
+++ index.php
@@ -0,0 +1,334 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.7.4/css/mdb.min.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <div class="container">
+        <div class="header">
+            <div class="rows">
+                <div class="logo">
+                    <img src="/img/logo.PNG" alt="">
+                </div>
+                <div class="both">
+                    <div class="social">
+
+                        <div class="search">
+                            <input type="text" name="search" id="search" class="">
+                        </div>
+                        <div class="icons">
+                            <i class="fas fa-search"></i>
+                            <i class="fab fa-facebook-f"></i>
+                            <i class="fab fa-twitter"></i>
+                            <i class="fab fa-youtube"></i>
+                            <i class="fab fa-pinterest"></i>
+                            <i class="fab fa-instagram"></i>
+                            <i class="dbtn">DONATE</i>
+                        </div>
+                    </div>
+                    <div class="navb ">
+                        <nav class="navbar navbar-expand-lg justify-content-center">
+                            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                                <span class="navbar-toggler-icon"></span>
+                                menu
+                            </button>
+                            <div class="collapse navbar-collapse" id="navbarNav">
+                                <ul class="navbar-nav">
+                                    <li class="nav-item active">
+                                        <a class="nav-link" href="#"> <b>Volunteer</b> <span class="sr-only"></span></a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#"><b>Shop</b></a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#"><b>Support</b></a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#">Where we build</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#">Impact</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#">Housing Help</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- section2 -->
+        <div class="main-img">
+            <div class="main-inner">
+                <div class="don">
+                    <h1>Every <br> Donation</h1>
+                    <h4>helps a family build a safe and <br> affordable home.</h4>
+                    <h4 class="btn">Donate Now</h4>
+                </div>
+            </div>
+        </div>
+        <div class="spacer">
+
+        </div>
+        <!-- sction3 -->
+        <div class="section3">
+            <div class="col-md-12">
+                <div class="row">
+                    <div class="col-md-3">
+                        <img class="home-img" src="/img/home.png" alt="">
+                    </div>
+                    <div class="col-md-9">
+                        <h1 class="boldh1">We build strength, stability and self-reliance through shelter.</h1>
+                        <p>Habitat for Humanity is a nonprofit organization that helps families build and improve places to call home. We believe affordable housing plays a critical role in strong
+                            and stable communities.</p>
+                        <button type="button" class="btn aw">Learn About Our Work</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="spacer">
+
+        </div>
+        <!-- section4 -->
+        <div class="section4">
+            <div class="row">
+                <div class="grid">
+                    <div class="first">
+                        <i class="fas fa-allergies"></i>
+                        <div class="inner">
+                            <h1>Find your local Habitat</h1>
+                            <p>Find a Habitat near you to volunteer, shop at a Habitat ReStore and more.</p>
+                            <input type="text" name="" id="" class="zipcode" placeholder="Zip Code">
+                            <i class="zip-search">Search Now</i> <br>
+                            <a href="#" class="more"> More search options →</a>
+                        </div>
+                    </div>
+                    <div class="second">
+                        <i class="far fa-heart"></i>
+                        <div class="inner">
+                            <h1>Donate</h1>
+                            <p>Families who partner with us build their own homes alongside volunteers, pay an affordable mortgage and are grateful for your help.</p>
+                            <input type="text" name="" id="" class="donate" placeholder="$25">
+                            <i class="zip-search">Donate</i> <br>
+                        </div>
+                    </div>
+                    <div class="third">
+                        <div class="third-inner">
+                            <h1>How to qualify <br> for a house</h1>
+                            <h4 class="btn">Learn more</h4>
+                        </div>
+                    </div>
+                </div>
+                <div class="grid1">
+                    <div class="g1">
+                        <img src="/img/promo.jpeg" alt="">
+                    </div>
+                    <div class="gs">
+                        <i class="fab fa-alipay"></i>
+                        <div class="inner">
+                            <h1>Subscribe</h1>
+                            <p>Families who partner with us build their own homes alongside volunteers, pay an affordable mortgage and are grateful for your help.</p>
+                            <input type="text" name="" id="" class="Subscribe" placeholder="EMAIL HERE">
+                            <i class="zip-search">Subscribe</i> <br>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="spacer"></div>
+        <!-- section5 -->
+        <div class="section5">
+            <h1>Habitat features</h1>
+            <div class="row">
+                <div class="card col">
+                    <div class="view overlay">
+                        <img class="card-img-top" src="/img/pexels-photo-1959055.jpeg" alt="Card image cap">
+                        <a href="#!">
+                            <div class="mask rgba-white-slight"></div>
+                        </a>
+                    </div>
+                    <div class="card-body card-bg2">
+                        <h4 class="card-title">Carters and Kims come full-circle with Habitat</h4>
+                        <p class="card-text">The lives of Jimmy and Rosalynn Carter have intertwined with those of Peter and Kathryn Kim in extraordinary ways, from their mutual dedication to Habitat
+                            to Peter's hand in helping bring a happy end to President Carter’s cancer journey.</p>
+                        <a href="#" class="btn btn-white">Read More</a>
+
+                    </div>
+
+                </div>
+                <div class="card col ">
+                    <div class="view overlay">
+                        <img class="card-img-top" src="/img/pexels-photo-1979261.jpeg" alt="Card image cap">
+                        <a href="#!">
+                            <div class="mask rgba-white-slight"></div>
+                        </a>
+                    </div>
+                    <div class="card-body card-bg1">
+                        <h4 class="card-title">Tools that build a better future</h4>
+                        <p class="card-text">The lives of Jimmy and Rosalynn Carter have intertwined with those of Peter and Kathryn Kim in extraordinary ways, from their mutual dedication to Habitat
+                            to Peter's hand in helping bring a happy end to President Carter’s cancer journey.</p>
+                        <a href="#" class="btn btn-white">Read More</a>
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        <div class="spacer"></div>
+        <!-- 3card -->
+        <div class="lastcards">
+            <div class="row">
+                <div class="card col">
+                    <div class="view overlay">
+                        <img class="card-img-top" src="/img/pexels-photo-1959055.jpeg" alt="Card image cap">
+                        <a href="#!">
+                            <div class="mask rgba-white-slight"></div>
+                        </a>
+                    </div>
+                    <div class="card-body card-bg1">
+                        <h4 class="card-title">Carters and Kims come full-circle with Habitat</h4>
+                        <p class="card-text">The lives of Jimmy and Rosalynn Carter have intertwined with those of Peter and Kathryn Kim in extraordinary ways, from their mutual dedication to Habitat
+                            to Peter's hand in helping bring a happy end to President Carter’s cancer journey.</p>
+                        <a href="#" class="btn btn-white">Read More</a>
+
+                    </div>
+
+                </div>
+                <div class="card col">
+                    <div class="view overlay">
+                        <img class="card-img-top" src="/img/pexels-photo-1959055.jpeg" alt="Card image cap">
+                        <a href="#!">
+                            <div class="mask rgba-white-slight"></div>
+                        </a>
+                    </div>
+                    <div class="card-body card-bg2">
+                        <h4 class="card-title">Carters and Kims come full-circle with Habitat</h4>
+                        <p class="card-text">The lives of Jimmy and Rosalynn Carter have intertwined with those of Peter and Kathryn Kim in extraordinary ways, from their mutual dedication to Habitat
+                            to Peter's hand in helping bring a happy end to President Carter’s cancer journey.</p>
+                        <a href="#" class="btn btn-white">Read More</a>
+
+                    </div>
+
+                </div>
+                <div class="card col">
+                    <div class="view overlay">
+                        <img class="card-img-top" src="/img/pexels-photo-1959055.jpeg" alt="Card image cap">
+                        <a href="#!">
+                            <div class="mask rgba-white-slight"></div>
+                        </a>
+                    </div>
+                    <div class="card-body card-bg4">
+                        <h4 class="card-title">Carters and Kims come full-circle with Habitat</h4>
+                        <p class="card-text">The lives of Jimmy and Rosalynn Carter have intertwined with those of Peter and Kathryn Kim in extraordinary ways, from their mutual dedication to Habitat
+                            to Peter's hand in helping bring a happy end to President Carter’s cancer journey.</p>
+                        <a href="#" class="btn btn-white">Read More</a>
+
+                    </div>
+
+                </div>
+            </div>
+            <div class="spacer"></div>
+            <div class="center">
+                <a href="#" class="btn btn-white center">Read More</a>
+            </div>
+        </div>
+        <div class="spacer"></div>
+        <div class="col-md-12">
+            <div class="row">
+                <div class="col-md-3">
+
+                </div>
+                <div class="col-md-9">
+                    <div class="social">
+
+                        <div class="search">
+                            <input type="text" name="search" id="search" class="" placeholder="looking for something?">
+                        </div>
+                        <div class="icons">
+                            <i class="fas fa-search"></i>
+                            <i class="fab fa-facebook-f"></i>
+                            <i class="fab fa-twitter"></i>
+                            <i class="fab fa-youtube"></i>
+                            <i class="fab fa-pinterest"></i>
+                            <i class="fab fa-instagram"></i>
+                            <i class="dbtn">DONATE</i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="spacer"></div>
+        <div class="spacer"></div>
+<!-- Footer -->
+<footer class="page-footer font-small black pt-4">
+        <div class="container-fluid text-center text-md-left">
+          <div class="row">
+            <div class="col-md-4 mt-md-0 mt-3">
+                <img class="foot-logo" src="/img/logo.PNG" alt="">
+            <div class="row">
+              <ul class="foot-menu">
+                <li>   Support</li>
+                <li> Donate one time</li>
+                <li>aDonate monthlydghjas</li>
+                <li>Partnerships</li>
+                <li>Donate goods</li>
+              </ul>
+              <ul class="foot-menu">
+                <li>Contact Us</li>
+                <li>Newsroom</li>
+                <li> Careers</li>
+                <li>Privacy policy</li>
+                <li>Terms of service</li>
+              </ul>
+            </div>
+            </div>
+            <hr class="clearfix w-100 d-md-none pb-3">
+            <div class="col-md-8">
+                <div class="col-md-12">
+                    <ul class="foot-smenu">
+                        <li class="foot-smenu-item"> Volunteer</li>
+                        <li class="foot-smenu-item">Shop</li>
+                        <li class="foot-smenu-item">Support</li>
+                        <li class="foot-smenu-item">Where We Build</li>
+                        <li class="foot-smenu-item">Impact</li>
+                        <li class="foot-smenu-item">Housing Help</li>
+                        <li class="foot-smenu-item">About</li>
+                    </ul>
+                </div>
+                <div class="col-md-12">
+                    <p>© 2019 Habitat for Humanity® International. <br>
+<br>
+                            All rights reserved. "Habitat for Humanity®" is a registered service mark owned by Habitat for Humanity International. Habitat® is a service mark of Habitat for Humanity International. <br>
+                            <br>
+                            Habitat for Humanity® International is a tax-exempt 501(C)(3) nonprofit organization. Your gift is tax-deductible as allowed by law.</p> 
+                </div>
+
+            </div>
+        </div>
+ 
+        <div class="footer-copyright text-center py-3">© 2018 Copyright:
+          <a href="https://mdbootstrap.com/education/bootstrap/"> RAVi</a>
+        </div>
+      </footer>
+
+    </div>
+
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.7.4/js/mdb.min.js"></script>
+</body>
+
+</html>
